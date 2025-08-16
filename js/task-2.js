6d2e7ac0087125ee9f27f9c56508dcbe0a9f1bc1
Maksim <70702008m@gmail.com>
2025-08-16 08:59:02 +0300

    text

---
+++ js/task-2.js
@@ -0,0 +1,20 @@
+class Storage{
+    #items;
+
+     constructor(initialItems) {
+    this.#items = initialItems;
+  }
+
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    this.#items = this.#items.filter(item => item !== itemToRemove);
+  }
+}
+
